ef08836108d2f4c21c8c76a11c4e6488b8fe0ee6
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 18:20:48 +0300

    Add route resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index]
+post '/signup', to: 'users#create' 
+post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
 get '/me', to: 'sessions#show'
 
   # Defines the root path route ("/")
