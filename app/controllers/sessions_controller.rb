bf98834e03c3d2a9efa786072e70d473875017fe
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 18:14:30 +0300

    Show action for remebering loged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,3 +7,12 @@
             render json: {errors: "Invalid username or password"}
         end
     end
+
+    def show 
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        else
+            render json: {errors: "Invalid user"}
+        end
+    end
