d761f26d35115630b8d7769a1438f80e878cca8b
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 18:12:08 +0300

    Create destroy rout to handle logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-post '/signup', to: 'users#create' 
-post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
 
   # Defines the root path route ("/")
   # root "articles#index"
