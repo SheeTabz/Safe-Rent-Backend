da05ac78bf03c3ae5c070ef852cedea23db86d5e
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 18:17:29 +0300

    Create action to store data of new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-def index 
-    user = User.all
-    render json: user
-end
+
+    def create 
+        user = User.create(user_params)
+        if user.valid?
+            # session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
 end
