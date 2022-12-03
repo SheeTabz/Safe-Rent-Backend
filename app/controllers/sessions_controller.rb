2929fbaa20a15451b825df1c75a10fdea65c4af9
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-03 18:13:52 +0300

    Create action for user login and store sessioninfo

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+    def create 
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: user
+        else 
+            render json: {errors: "Invalid username or password"}
+        end
+    end
