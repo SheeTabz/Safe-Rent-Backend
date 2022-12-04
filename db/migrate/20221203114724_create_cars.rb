fa5a1cae41c95f71dc8bf84c0f1bad2016c3ff3a
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-04 14:36:34 +0300

    Create reviews and car migratins

--- db/migrate/20221203114724_create_cars.rb
+++ db/migrate/20221203114724_create_cars.rb
@@ -2,7 +2,7 @@ class CreateCars < ActiveRecord::Migration[7.0]
   def change
     create_table :cars do |t|
       t.string :name
-      t.string :class
+      t.string :level
       t.string :conditioning
       t.string :transmision
       t.integer :price
