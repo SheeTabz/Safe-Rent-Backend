40133552c720aa9fe6a53246ba56ccc11e802e74
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-04 14:37:05 +0300

    Add car controller action

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,53 @@
 class CarsController < ApplicationController
-
+# before_action :authorize
+# skip_before_action :authorize, only: [:index]
     def index 
         cars = Car.all 
         render json: cars
     end
-end
+
+    def create 
+        cars = Car.create(cars_params)
+        if cars.valid?
+        render json: cars
+        else 
+            render json: {error: "Invalid record type"}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        car = Car.find(params[:id])
+        car.delete
+        head:no_content
+    end
+
+
+    def show 
+        car = Car.find_by(id: params[:id])
+        if car
+            render json: car, include: ['reviews', 'reviews.user']
+        else 
+            render json: {errors: "Could not find car with that id"}
+        end
+    end
+
+    def update
+        car = Car.find_by(id: params[:id])
+        if car
+          car.update(cars_params)
+          render json: car
+        else
+          render json: { error: "Car not found" }, status: :not_found
+        end
+      end
+    private 
+
+    def cars_params
+        params.permit(:name, :level, :conditioning, :transmision, :price, :image_url)
+    end
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+      end
+
+    end
