8d36279ff4349b44ed3b5bef05779780105d9e2d
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-04 14:40:35 +0300

    User , cars, reviews resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
 post '/signup', to: 'users#create' 
 post '/login', to: 'sessions#create'
 delete '/logout', to: 'sessions#destroy'
 get '/me', to: 'sessions#show'
-
-resources :cars, only: [:index, :create]
+resources :reviews
+resources :cars, only: [:index, :create, :destroy, :show, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
