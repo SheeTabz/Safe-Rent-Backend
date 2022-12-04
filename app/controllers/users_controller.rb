9dee76da748c45bcb9c94cd1107659196f74e2dd
SheeTabz <wmuriithitabitha@gmail.com>
2022-12-04 14:38:04 +0300

    Update users action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,13 @@ end
     def create 
         user = User.create(user_params)
         if user.valid?
-            # session[:user_id] = user.id
+            session[:user_id] = user.id
             render json: user, status: :created
         else
             render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
         end
     end
+
 
    
 
